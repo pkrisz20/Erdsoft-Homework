78e246135702cefff2b0bff5584934c200e0f7e1
pkrisz20 <krisztianpasztor11@gmail.com>
2021-07-06 17:15:00 +0200

    Init

---
+++ laravel/resources/blade.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>A birtok</title>
+
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/app.css">
+
+</head>
+<body>
+
+<nav class="navbar navbar-expand-lg whole">
+<div class="container">
+  <a class="navbar-brand" href="#"><img width="120px" heiight="120px" src="logo.png"></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"><i class="fas fa-bars" style="color:#fff; font-size:28px;"></i></span>
+  </button>
+
+  <div class="icons"><a class="sign-in" href="#"><i class="fas fa-user"></i> BELEPES</a> | <a href="#">HU</a> | <a href="#">EN</a></div>
+  <div class="collapse navbar-collapse menus col-lg-10" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link hover" href="#">NYITOLAP </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link hover" href="#">A BIRTOK</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link hover" href="#">WEBSHOP</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link hover" href="#">ETTERMEK</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link hover" href="#">BORKOSTOLO</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link hover" href="#">HIREK</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link hover" href="#">KAPCSOLAT</a>
+      </li>
+    </ul>
+
+    <button class="button">KOSAR <i class="fas fa-shopping-cart"></i></button>
+    <div class="purple">PURPLE</div>
+  </div>
+  </div>
+</nav>
+
+
+
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+</html>
