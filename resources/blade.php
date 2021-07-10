9307541ef5d25452c8f3c75f7e798ba174d5f5e7
pkrisz20 <krisztianpasztor11@gmail.com>
2021-07-10 15:44:45 +0200

    Second push

--- resources/blade.php
+++ resources/blade.php
@@ -11,20 +11,23 @@
     <link rel="stylesheet" href="css/app.css">
 
 </head>
-<body>
+
+<!--NAVBAR-->
 
 <nav class="navbar navbar-expand-lg whole">
 <div class="container">
-  <a class="navbar-brand" href="#"><img width="120px" heiight="120px" src="logo.png"></a>
+  <a class="navbar-brand" href="#"><img width="120px" src="logo.png"></a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"><i class="fas fa-bars" style="color:#fff; font-size:28px;"></i></span>
   </button>
 
-  <div class="icons"><a class="sign-in" href="#"><i class="fas fa-user"></i> BELEPES</a> | <a href="#">HU</a> | <a href="#">EN</a></div>
+  <!--LOGO + LINKS-->
+
+  <div class="icons"><a class="sign-in" href="#"><i class="fas fa-user"></i> BELÉPÉS</a> | <a href="#">HU</a> | <a href="#">EN</a></div>
   <div class="collapse navbar-collapse menus col-lg-10" id="navbarNav">
     <ul class="navbar-nav">
       <li class="nav-item">
-        <a class="nav-link hover" href="#">NYITOLAP </a>
+        <a class="nav-link hover" href="#">NYITÓLAP </a>
       </li>
       <li class="nav-item">
         <a class="nav-link hover" href="#">A BIRTOK</a>
@@ -33,10 +36,10 @@
         <a class="nav-link hover" href="#">WEBSHOP</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link hover" href="#">ETTERMEK</a>
+        <a class="nav-link hover" href="#">ÉTTERMEK</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link hover" href="#">BORKOSTOLO</a>
+        <a class="nav-link hover" href="#">BORKÓSTOLÓ</a>
       </li>
       <li class="nav-item">
         <a class="nav-link hover" href="#">HIREK</a>
@@ -46,12 +49,78 @@
       </li>
     </ul>
 
-    <button class="button">KOSAR <i class="fas fa-shopping-cart"></i></button>
-    <div class="purple">PURPLE</div>
+    <button class="button">KOSÁR <i class="fas fa-shopping-cart"></i></button>
+
   </div>
+  
   </div>
 </nav>
 
+<!--HEADER IMAGE-->
+<body>
+<div class="container-fluid">
+<div class="centered">A birtok</div>
+
+  <div class="row header-div">
+    <img class="header " src="header-edited.jpg">
+  </div>
+
+  <div class="row black-card mb-4 col-xl-5 col-xxl-4 col-md-8 col-sm-10 col-12">
+
+    <div class="card">
+      <div class="card-body">
+        <div class="title-div"> <h1 class="card-title mb-3">Történet</h1> </div>
+        <h6 class="card-subtitle mb-3">Magyarország elsőrangú és kivételes borainak legjava.</h6>
+        <p class="card-text">A Váradi család vaxco birtoka Mádon, az UNESCO világörökség részévé nyilváníitott, történelmi, Tokaji borvidék területén található – egy kivételesen gazdag terroir, amely különféle termőtalajok találkozásából keletkezett. A Juliet Victor sokrétű világa megteremti a tökéletes egyensúlyt történelmi és modern között: nagy múltú terroir és csúcstechnológiát alkalmazó előállítás. A Juliet Victor elegáns, száraz fehér és igazán egyedülálló, édes desszertborokat alkot a bor szerelmeseinek.</p>
+        <a href="#" class="card-link">TOVÁBB &nbsp &nbsp<i class="fas fa-chevron-right"></i></a>
+      </div>
+    </div>
+
+  </div>
+</div>
+
+<div class="container-fulid">
+  <div class="merged-div w-100">
+    <img class="merged" src="merged.png">
+  </div>
+
+  <div class="row winery">
+
+    <div class="column">
+      <img src="winery.jpg">
+    </div>
+
+    <div class="column">
+
+      <div class="card1">
+        <div class="card1-body">
+          <div class="title1-div"> <h1 class="card1-title mb-3">A borászat</h1> </div>
+          <p class="card1-text">Mád, mint Tokaj-Hegyalja egyik jelentős történelmi települése, a borvidék meghatározó szőlőterületeivel rendelkezik. E területek változatos vulkanikus talaja, ideális fekvése és egyedi mikroklímája révén magas minőségű, izgalmas borok megszületésének adnak lehetőséget. A borok létrejöttének a szőlőterület, a szőlő és az ezeket kezelő, évszázadok alatt csiszolódott emberi tudás mellett, egy fontos eleme még a borászati épület is. Hegyaljára jellemző módon Mádon is e borászatok többségében nem kint a szőlőben, hanem a település történelmi szerkezetében helyezkednek el, beleilleszkedve a település szövetébe, lakó- és különféle középületek szomszédságában.</p>
+          <a href="#" class="card1-link">TOVÁBB &nbsp &nbsp<i class="fas fa-chevron-right"></i></a>
+        </div>
+      </div>
+
+    </div>
+
+  </div>
+</div>
+
+<div class="container">
+  <div class="row two-images">
+
+      <div class="card2">
+        <div class="card2-body">
+          <div class="title2-div"> <h1 class="card2-title mb-3">A terület</h1> </div>
+          <p class="card2-text">A Juliet Victor számára a világhírű Tokaji borvidék szívében található Mád történelmi községe tökéletes éghajlati feltételeket, termőtalajt és bortermelő területet biztosít, amely az egyedülálló erjesztési és érlelési folyamatok képességének, mesterségbeli tudásának is táptalajává válik.</p>
+        </div>
+      </div>
+
+      <div>
+        <img class="territory" src="territory.jpg">
+      </div>
+
+  </div>
+</div>
 
 
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
